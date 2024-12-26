e488c9bfb3c21d37717dca373d22757f571b21ab
albert912 <aatunckol@gmail.com>
2024-12-26 21:58:03 +0300

    Create an object that will handle the display/DOM logic

---
+++ uitictactoe.js
@@ -0,0 +1,143 @@
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  /* const dropToken = (column, player) => {
+    const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
+
+    if (!availableCells.length) return;
+
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addToken(player);
+  }; */
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, /* dropToken, */ printBoard };
+}
+
+function Cell() {
+  let value = 0;
+
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue
+  };
+}
+
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = Gameboard();
+
+  const players = [
+    {
+      name: playerOneName,
+      token: 1
+    },
+    {
+      name: playerTwoName,
+      token: 2
+    }
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (column) => {
+    console.log(
+      `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+    );
+    // board.dropToken(column, getActivePlayer().token);
+
+    /* This is where we would check for a winner and handle that logic,
+       such as a win message. */
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+    getBoard: board.getBoard
+  };
+}
+
+function ScreenController() {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector('.turn');
+  const boardDiv = document.querySelector('.board');
+
+  const updateScreen = () => {
+    // Clear the board
+    boardDiv.textContent = "";
+
+    // Get the latest board and active player
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display the current player's turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+    // Render board cells
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, columnIndex) => {
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        cellButton.dataset.column = columnIndex;
+        cellButton.textContent = cell.getValue(); // Display cell value
+        boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  // Add event listener for the board
+  /* function clickHandlerBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn) return;
+
+    game.playRound(selectedColumn);
+    updateScreen();
+  }
+
+  boardDiv.addEventListener("click", clickHandlerBoard); */
+
+  // Initial render
+  updateScreen();
+}
+
+ScreenController();
